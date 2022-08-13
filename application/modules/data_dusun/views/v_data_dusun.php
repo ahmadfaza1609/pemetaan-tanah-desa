71c2e2b2c3051549768da02d872bacc0c8f8149d
ahmadfaza1609 <ahmad.fazahazmi@gmail.com>
2022-08-13 07:54:58 +0700

    update dusun models

--- application/modules/data_dusun/views/v_data_dusun.php
+++ application/modules/data_dusun/views/v_data_dusun.php
@@ -13,7 +13,7 @@
                     Data Dusun Desa Bantan Tengah yang terdaftar di pemerintahan desa.
                 </p>
                 <div class="table-wrapper table-responsive">
-                    <table class="table">
+                    <table id="table" class="table">
                         <thead>
                             <tr>
                                 <th>
